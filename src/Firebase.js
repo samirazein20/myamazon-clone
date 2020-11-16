97be866686aa9ad54431d605b1ac638953df1622
Samira Zein <samirazein20@gmail.com>
2020-11-16 17:18:45 -0600

    Amazon header bar component  implementation

---
+++ src/Firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC5Zs_3aJ5X4-J1S7qiVeFlj7TRr-ZMyJk",
+    authDomain: "myamzon-clone.firebaseapp.com",
+    databaseURL: "https://myamzon-clone.firebaseio.com",
+    projectId: "myamzon-clone",
+    storageBucket: "myamzon-clone.appspot.com",
+    messagingSenderId: "9432157924",
+    appId: "1:9432157924:web:0d0110ee224330a87366f1",
+    measurementId: "G-NBM8X4Q7EV"
+};
